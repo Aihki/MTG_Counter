4a578fd55b8cbde602ac3ef79d952f1ce8d81872
aihki <laine.nestori.julius@gmail.com>
2024-08-10 12:41:08 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/MTG_counter/sw.js', { scope: '/MTG_counter/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/MTG_Counter/sw.js', { scope: '/MTG_Counter/' })})}
